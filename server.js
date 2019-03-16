bb3972c4e7884c1d16b66b6c9cbfd25b450e7d69
Arron Linton <lintonarron@gmail.com>
2019-03-16 16:08:10 -0700

    redirecturi

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize' +
     '?response_type=code' +
     '&client_id=0624a1c68afc4283863103888b6464c5&scope=' + encodeURIComponent(scopes) +
-    '&redirect_uri=https://git.heroku.com/spotify-jams.git');
+    '&redirect_uri=https://spotify-jams.herokuapp.com');
   });
 
 app.get("/spotify", (req, res) => {
