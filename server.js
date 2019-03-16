c16af64bf9a936b7b2799de8744a38cbbda3c87e
Arron Linton <lintonarron@gmail.com>
2019-03-16 16:16:47 -0700

    added spotify credentials

--- server.js
+++ server.js
@@ -21,22 +21,14 @@ var fs = require("fs");
 // Initialize the spotify API client using our client id and secret
 // var spotify = new Spotify(keys.spotify);
 var SpotifyApi = require('spotify-web-api-node');
-var spotify = new SpotifyApi({
-  clientId: '0624a1c68afc4283863103888b6464c5',
-  clientSecret: 'fe4d4619ed16422ba113b799eb1ad2ba',
-  redirectUri: 'http://spotify-jams.herokuapp.com/'
-});
 var express = require("express");
-
 var app = express();
-
 var cors = require('cors')
-
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors())
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // app.get('/', function(req, res) {
   
 //   var scopes = 'user-read-private user-read-email';
@@ -46,6 +38,22 @@ const PORT = process.env.PORT || 3000;
 //     '&client_id=0624a1c68afc4283863103888b6464c5&scope=' + encodeURIComponent(scopes) +
 //     '&redirect_uri=https://spotify-jams.herokuapp.com/spotify');
 //   });
+var spotify = new SpotifyApi({
+  clientId: '0624a1c68afc4283863103888b6464c5',
+  clientSecret: 'fe4d4619ed16422ba113b799eb1ad2ba',
+  redirectUri: 'http://spotify-jams.herokuapp.com/'
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  });
 
 app.get("/", (req, res) => {
  
