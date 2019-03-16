c454376133a7c62ae0e83f66432d6b7e8a6c0572
Arron Linton <lintonarron@gmail.com>
2019-03-16 16:18:17 -0700

    added spotify credentials

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ spotify.clientCredentialsGrant()
     console.log('The access token is ' + data.body['access_token']);
 
     // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
+    spotify.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err.message);
   });
