cb30b3b8c6f7d299f139a5e959bcc186c3d8de65
Arron Linton <lintonarron@gmail.com>
2019-03-17 09:55:04 -0700

    ui edit

--- app.js
+++ app.js
@@ -28,29 +28,64 @@ window.onSpotifyWebPlaybackSDKReady = () => {
   // player.connect();
 
 
-  const play = ({
-    spotify_uri,
-    playerInstance: {
-      _options: {
-        getOAuthToken,
-        id
-      }
-    }
-  }) => {
-    getOAuthToken(access_token => {
-      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ uris: [spotify_uri] }),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${access_token}`
-        },
-      });
-    });
-  };
+  // const play = ({
+  //   spotify_uri,
+  //   playerInstance: {
+  //     _options: {
+  //       getOAuthToken,
+  //       id
+  //     }
+  //   }
+  // }) => {
+
+  //     fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+  //       method: 'PUT',
+  //       body: JSON.stringify({ uris: [spotify_uri] }),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `Bearer BQDA7SA4Jt5Oi6GFtiivFnHxe7CxGVVgFNjX6_vC4xUVkXx88R-a_mJmtX4U0I79BLw2Ll1zwqzVfKuh6CU`
+  //       },
+  //     });
+  //   };
+
   
-  play({
-    playerInstance: new Spotify.Player({ name: "Arron J" }),
-    spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
-  });
+
+  // play({
+  //   playerInstance: new Spotify.Player({ name: "Arron J" }),
+  //   spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+  // });
+
+
+  	
+
+
+// Code Sample	
+var player = new Spotify.Player({
+  name: 'Arron J',
+  getOAuthToken: callback => {
+    // Run code to get a fresh access token
+
+    callback('BQCU6L73BpbZP9Qwt1UtOWfMbLg-gvAgS4q95PHHmj0t0x6tvNzIx_lex7lbenN0DyJ2Dtekb_ikgNuHOEU');
+  },
+  volume: 0.5
+});
+
+player.connect().then(success => {
+  if (!success) {
+    console.log('The Web Playback SDK successfully connected to Spotify!');
+  }
+
+  player.addListener('ready', ({ device_id }) => {
+    console.log('The Web Playback SDK is ready to play music!');
+    console.log('Device ID', device_id);
+    player.nextTrack().then(() => {
+      console.log('Skipped to next track!');
+    });
+  })
+  
+
+  
+})
+
+
 };
--- server.js
+++ server.js
@@ -3,41 +3,18 @@
 // Read and set environment variables
 require("dotenv").config();
 
-// Import the node-spotify-api NPM package.
-// var Spotify = require("node-spotify-api");
-
-// Import the API keys
-var keys = require("./keys");
-
-// Import the axios npm package.
-var axios = require("axios");
-
-// Import the moment npm package.
-var moment = require("moment");
-
-// Import the FS package for read/write.
-var fs = require("fs");
-
 // Initialize the spotify API client using our client id and secret
 // var spotify = new Spotify(keys.spotify);
-var SpotifyApi = require('spotify-web-api-node');
 var express = require("express");
 var app = express();
 var cors = require('cors')
+var SpotifyApi = require('spotify-web-api-node');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.get('/', function(req, res) {
-  
-//   var scopes = 'user-read-private user-read-email';
-//   const redirect_uri = "https://git.heroku.com/spotify-jams.git"
-//   res.redirect('https://accounts.spotify.com/authorize' +
-//     '?response_type=code' +
-//     '&client_id=0624a1c68afc4283863103888b6464c5&scope=' + encodeURIComponent(scopes) +
-//     '&redirect_uri=https://spotify-jams.herokuapp.com/spotify');
-//   });
+
 var spotify = new SpotifyApi({
   clientId: '0624a1c68afc4283863103888b6464c5',
   clientSecret: 'fe4d4619ed16422ba113b799eb1ad2ba',
@@ -55,7 +32,10 @@ spotify.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err.message);
   });
 
+
 app.get("/:playlist", (req, res) => {
+
+  console.log(req.headers)
  
 // sample -> 5ieJqeLJjjI8iJWaxeBLuK
   let playlist = req.params.playlist
